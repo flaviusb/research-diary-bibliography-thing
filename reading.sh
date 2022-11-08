a9bfb46a8d403a81c50496b46dcac2fd9d3f7a01
Justin (:flaviusb) Marsh <justin.marsh@flaviusb.net>
2022-11-08 16:36:24 +1300

    Make reading.sh able to open multiple paper lists at once.

--- reading.sh
+++ reading.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-echo `cat "$1" | awk '/^.*$/ { x = gensub(/^([^#]*)(#.*)?$/, "\\\\1", "1"); print (x) }'` | xargs evince
+while [[ $# > 0 ]]; do
+  if [[ -e $1 ]];
+  then
+    echo `cat "$1" | awk '/^.*$/ { x = gensub(/^([^#]*)(#.*)?$/, "\\\\1", "1"); print (x) }'` | xargs evince &
+  fi
+  shift
+done
